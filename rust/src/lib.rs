9d37a649ca4c67710ef74c379547069eab3bb14d
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2022-12-15 09:21:01 -0500

    initial commit - testing

---
+++ rust/src/lib.rs
@@ -0,0 +1,6 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(js_name="add")]
+pub fn add(a: u32, b:u32) -> u32 {
+    a + b
+}
---
+++ web/src/routes/add/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import init, { add} from "../../wasm/pkg/rust.js"
+    let cumulative = 0
+
+    const add5 = () => {
+        const n = add(cumulative, 5)
+        cumulative = n
+    }
+    
+
+</script>
+
+<div>
+    <h3>{cumulative}</h3>
+    <button on:click={add5}>+ 5</button>
+</div>
